9100ff3066934cdcbd2b5d7f7401d7192be5c136
msk1039 <mayankkadam1039@gmail.com>
2024-01-27 20:14:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Mybutton from "./Mybutton";
+function App() {
+  return <div>
+      <h1>Hello React</h1>
+      <Mybutton></Mybutton>
+  </div>
+  
+}
+export default App;
---
+++ src/Mybutton.jsx
@@ -0,0 +1,4 @@
+function Mybutton(){
+  return <button>Click Me</button>
+}
+export default Mybutton;
